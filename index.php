11ed51fb666c6c1c2092170ca415a562b4ba2365
Christina Sosa <csosa2106@gmail.com>
2016-07-12 16:50:27 -0600

    added HTML framework and comments

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8"/ >
+
+		<!--adding custom CSS-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+		<!--adding custom JavaScript-->
+		<script src="js/custom.js" type="text/javascript"></script>
+
+		<title>All About Me</title>
+
+	</head>
+	<!--main body begins-->
+	<body>
+		<header>About Me</header>
+		<main>
+<h1>Learn more about me!</h1>
+			<h2>Welcome to my site! Read below to learn more about <em>ME</em>!</h2>
+			<p>Hello, my name is Christina. I'm originally from Brooklyn, New York but have lived in New Mexico for
+				many years now. </p>
+			<p>Don't be lasagna. Black tie...Whenever I wear this, something bad always happens. Exotic alien swords are easy to come by, Aces are rare. Frightened people. Give me a Dalek any day. Aw, I wanted to be ginger! I've never been ginger! And you, Rose Tyler! Fat lot of good you were! You gave up on me! Ooh, that's rude. Is that the sort of man I am now? Am I rude? Rude and not ginger. You can always judge a man by the quality of his enemies. Yeah, I came first in jiggery pokery, what about you? My last incarnation…oh, I was never happy with that one. It had a sort of feckless 'charm' which simply wasn't me.</p>
+
+				<p>Your hips are fine, you're built like a man! Can I talk about planets now? Come on! Oh, yes. Harmless is just the word: that's why I like it! Doesn't kill, doesn't wound, doesn't maim. But I'll tell you what it does do: it is very good at opening doors! I don't want to go. I don't suppose we'll ever know if we actually succeeded. But at worst, we failed doing the right thing, as opposed to succeeding in doing the wrong. I hate computers and refuse to be bullied by them. Now would you stand aside before I shoot myself?</p>
+
+				<p>It all started out as a mild curiosity in the junkyard, and now it's turned out to be quite a great spirit of adventure. We are always in trouble! Isn't it extraordinary—it follows us everywhere. Well, you're very similar heights. Maybe you should wear labels. The thing is, Adam, time travel is like visiting Paris. You can't just read the guidebook, you've got to throw yourself in! Eat the food, use the wrong verbs, get charged double and end up kissing complete strangers! Or is that just me?</p>
+		</main>
+		<!--footer with copyright-->
+		<footer>&copy 2016</footer>
+	</body>
+</html>
