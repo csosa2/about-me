e0773448d981ebdfb45cda78ab8a56cf22532443
Christina Sosa <csosa2106@gmail.com>
2016-07-13 09:18:03 -0600

    added more HTML and style to CSS

--- index.php
+++ index.php
@@ -15,23 +15,36 @@
 	<!--main body begins-->
 	<body>
 		<header>
-			<h1>Learn more about me!</h1>
+			<h1>
+				Learn more about me!
+			</h1>
 		</header>
 		<main>
-			<h2>Welcome to my site! Read below to learn more about <em>ME</em>!</h2>
-			<p>Hello, my name is Christina. That's me. <img src="images/profile.jpg" alt="profile"/> </p>
-			<p>I'm originally from Brooklyn, New York but have lived in New Mexico for many years now. However, my 
-				family is originally from Mexico. </p>
+			<h2>
+				Welcome to my site! Read below to learn more about <em>ME</em>!
+			</h2>
+			<p>
+				Hello, my name is Christina. That's me. <img src="images/profile.jpg" alt="profile"/>
+			</p>
+			<p>
+				I'm originally from Brooklyn, New York but have lived in New Mexico for many years now. However, my
+				family is originally from Mexico.
+			</p>
 			<img src="images/mexicoFlag.jpg" alt="Mexican Flag"/>
-			<p>I recently took a trip to Mexico to visit my family <img src="images/family.jpg" alt="Family in Mexico">
+			<p>
+				I recently took a trip to Mexico to visit my family <img src="images/family.jpg" alt="Family in Mexico">
 				and eat some amazing food <img src="images/food.jpg" alt="Mexican Food">I was even fortunate enough to
-				visit some pyramids! <img src="images/pyramidTop.jpg" alt="Top of Pyramid"></p>
-			
-			<p>Don't be lasagna. Black tie...Whenever I wear this, something bad always happens. Exotic alien swords are easy to come by, Aces are rare. Frightened people. Give me a Dalek any day. Aw, I wanted to be ginger! I've never been ginger! And you, Rose Tyler! Fat lot of good you were! You gave up on me! Ooh, that's rude. Is that the sort of man I am now? Am I rude? Rude and not ginger. You can always judge a man by the quality of his enemies. Yeah, I came first in jiggery pokery, what about you? My last incarnation…oh, I was never happy with that one. It had a sort of feckless 'charm' which simply wasn't me.</p>
+				visit some pyramids! <img src="images/pyramidTop.jpg" alt="Top of Pyramid">
+			</p>
+			<p>
+				When not traveling, I like to spend time with my two best friends. That's them! <img src="images/stephanie.jpg" alt="Best Friend Stephanie"> 				<img src="images/kendall.jpg" alt="Best Friend
+				 Kendall">. The <em>Walking Dead</em> is my favorite show and I never miss the opportunity to watch
+				with my dog, Jasmine. <img src="images/watchShows.jpg" alt="Watching show with dog"> We both get really
+				into the show. For fun, I like to powerlift. I've only been doing it for a couple years, but have learned
+				so much about my own strength and how to push through mental boundaries. I even competed and got this
+				cool medal. <img src="images/powerlifting.jpg" alt="Powerlifting Medal">
+			</p>
 
-				<p>Your hips are fine, you're built like a man! Can I talk about planets now? Come on! Oh, yes. Harmless is just the word: that's why I like it! Doesn't kill, doesn't wound, doesn't maim. But I'll tell you what it does do: it is very good at opening doors! I don't want to go. I don't suppose we'll ever know if we actually succeeded. But at worst, we failed doing the right thing, as opposed to succeeding in doing the wrong. I hate computers and refuse to be bullied by them. Now would you stand aside before I shoot myself?</p>
-
-				<p>It all started out as a mild curiosity in the junkyard, and now it's turned out to be quite a great spirit of adventure. We are always in trouble! Isn't it extraordinary—it follows us everywhere. Well, you're very similar heights. Maybe you should wear labels. The thing is, Adam, time travel is like visiting Paris. You can't just read the guidebook, you've got to throw yourself in! Eat the food, use the wrong verbs, get charged double and end up kissing complete strangers! Or is that just me?</p>
 			<ul>
 				<li>
 					Favorite Thing 1
