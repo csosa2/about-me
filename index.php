4c8234833622304646d7ec460364b6ed490a6afc
Christina Sosa <csosa2106@gmail.com>
2016-07-15 00:02:17 -0600

    changed HTML order and added new CSS

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 	<html lang="en">
 	<head>
 		<meta charset="utf-8"/ >
-
+		<!--adding google fonts-->
+		<link href='https://fonts.googleapis.com/css?family=Athiti:400,700,600,500' rel='stylesheet' type='text/css'>
 		<!--adding custom CSS-->
 		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 
@@ -10,54 +11,74 @@
 		<script src="js/custom.js" type="text/javascript"></script>
 
 		<title>All About Me</title>
-
 	</head>
 	<!--main body begins-->
 	<body>
-		<header>
-			<h1>
-				Learn more about me!
-			</h1>
-		</header>
-		<main>
-			<h2>
-				Welcome to my site! Read below to learn more about <em>ME</em>!
-			</h2>
-			<p>
-				Hello, my name is Christina. That's me. <img src="images/profile.jpg" alt="profile"/>
-			</p>
-			<p>
-				I'm originally from Brooklyn, New York but have lived in New Mexico for many years now. However, my
-				family is originally from Mexico.
-			</p>
-			<img src="images/mexicoFlag.jpg" alt="Mexican Flag"/>
-			<p>
-				I recently took a trip to Mexico to visit my family <img src="images/family.jpg" alt="Family in Mexico">
-				and eat some amazing food <img src="images/food.jpg" alt="Mexican Food">I was even fortunate enough to
-				visit some pyramids! <img src="images/pyramidTop.jpg" alt="Top of Pyramid">
-			</p>
-			<p>
-				When not traveling, I like to spend time with my two best friends. That's them! <img src="images/stephanie.jpg" alt="Best Friend Stephanie"> 				<img src="images/kendall.jpg" alt="Best Friend
-				 Kendall">. The <em>Walking Dead</em> is my favorite show and I never miss the opportunity to watch
-				with my dog, Jasmine. <img src="images/watchShows.jpg" alt="Watching show with dog"> We both get really
-				into the show. For fun, I like to powerlift. I've only been doing it for a couple years, but have learned
-				so much about my own strength and how to push through mental boundaries. I even competed and got this
-				cool medal. <img src="images/powerlifting.jpg" alt="Powerlifting Medal">
-			</p>
-
-			<ul>
-				<li>
-					Favorite Thing 1
-				</li>
-				<li>
-					Favorite Thing 2
-				</li>
-				<li>
-					Favorite Thing 3
-				</li>
-			</ul>
-		</main>
-		<!--footer with copyright-->
-		<footer>&copy 2016</footer>
+			<header class="main-header">
+				<div class="container">
+					<h1 class="name">
+						About Me
+					</h1>
+					<h2>
+						Because I'm awesome like that.
+					</h2>
+				</div>
+			</header>
+			<main>
+				<div class="text-area">
+					<h2>
+						Welcome to my site! Read below to learn more about <em>ME</em>!
+					</h2>
+				<div>
+					<img src="images/pyramidTop.jpg" alt="Top of Pyramid">
+					<p>
+						Hello, my name is Christina. I'm originally from Brooklyn, New York but have lived in New Mexico for
+						many years now (thanks Intel!). Many people ask if I miss New York. In reality, I enjoy the open and
+						beautiful views that New Mexico has to offer. Plus the sunsets aren't too shabby. My family is
+						originally from Mexico and I love going there to visit, taste the amazing food and soak in the
+						beautiful culture. In my last trip, I got the opportunity to climb to the top of a pyramid!
+					</p>
+				</div>
+				<div>
+					<img src="images/watchShows.jpg" alt="Watching show with dog">
+					<p>
+						When not traveling, I like to spend time with my friends and family. I live with my amazing
+						grandmother and constantly get spoiled with her delightful Mexican cuisine. After a satisfying meal, I
+						enjoy watching	an episode of <em>The Walking Dead</em> with my dog, Jasmine. As you can see, she also
+						enjoys watching it with me. This was an intense episode!
+					</p>
+				</div>
+				<div>
+					<img src="images/powerlifter.jpeg" alt="Powerlifting">
+					<p>
+					For fun, I like to powerlift. Here are some things I learned through the sport:
+					</p>
+					<ul>
+						<li>
+							Powerlifting is more of a mental challenge than it is a physical challenge.
+						</li>
+						<li>
+							It’s okay to fail. It’s okay to miss a lift or have an off day of training. I've learned more from
+							failure than I have from success. I can look back and see where I went wrong and how I need to
+							improve.
+						</li>
+						<li>
+							It's cool to keep up with the boys. It's even cooler to be stronger than them.
+						</li>
+					</ul>
+					<p>
+						Last year I did something crazy. Something that I would have never done were it not for my coaches.
+						I competed in my first ever powerlifting meet. I never thought in a million years I would stand in
+						front of a large crowd, wearing a tight singlet, and have people intentionally stare at my
+						butt to see if I "hit depth" on my squat. It was one of the most exhausting days of my life, but I
+						made it through. I even set a state record in my class and got a cool medal!
+					</p>
+				</div>
+			</main>
+		</div>
+			<!--footer with copyright-->
+			<footer class="main-footer">
+				&copy 2016
+			</footer>
 	</body>
 </html>
